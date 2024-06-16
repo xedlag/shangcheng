f0f523632f06b6848e8d1da8e7f8abaef932f54e
xedlag <145640114+xedlag@users.noreply.github.com>
2024-06-17 04:18:32 +0800

    Create views.py
    Signed-off-by: xedlag <145640114+xedlag@users.noreply.github.com>

---
+++ backend/store/views.py
@@ -0,0 +1,27 @@
+# views.py
+# 定义 Django 应用的视图。
+
+from rest_framework import viewsets
+from .models import Product, Cart, CartItem, Order, OrderItem
+from .serializers import ProductSerializer, CartSerializer, CartItemSerializer, OrderSerializer, OrderItemSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+
+class CartItemViewSet(viewsets.ModelViewSet):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+
