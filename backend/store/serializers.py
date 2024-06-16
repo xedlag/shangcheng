4aeda19b1b31683b46db4a5e4bb930c6b1d1b9e0
xedlag <145640114+xedlag@users.noreply.github.com>
2024-06-17 04:16:52 +0800

    Create serializers.py
    Signed-off-by: xedlag <145640114+xedlag@users.noreply.github.com>

---
+++ backend/store/serializers.py
@@ -0,0 +1,36 @@
+# serializers.py
+# 定义 Django REST Framework 的序列化器。
+
+from rest_framework import serializers
+from .models import Product, Cart, CartItem, Order, OrderItem
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = '__all__'
+
+class CartSerializer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+  
