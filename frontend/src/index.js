69fcdeb121c7a96d0ae0f5b06a065dc3cff63335
xedlag <145640114+xedlag@users.noreply.github.com>
2024-06-17 04:24:02 +0800

    Update index.js
    Signed-off-by: xedlag <145640114+xedlag@users.noreply.github.com>

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,17 @@
+// index.js
+// React 应用的入口文件，渲染 App 组件。
+
+import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
