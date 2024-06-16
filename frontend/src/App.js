d6d62abd7f8b3044a7eb7608e0a31af5bceff492
xedlag <145640114+xedlag@users.noreply.github.com>
2024-06-17 04:23:38 +0800

    Update App.js
    Signed-off-by: xedlag <145640114+xedlag@users.noreply.github.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,24 @@
+// App.js
+// React 应用的主组件，定义路由和页面结构。
+
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ProductList from './components/ProductList';
+import Cart from './components/Cart';
+import OrderHistory from './components/OrderHistory';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={ProductList} />
+          <Route path="/cart" component={Cart} />
+          <Route path="/orders" component={OrderHistory} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
