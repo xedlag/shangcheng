9cd5b35197bee21a50e1089037bf90208090c4d7
xedlag <145640114+xedlag@users.noreply.github.com>
2024-06-17 04:13:48 +0800

    Update urls.py
    Signed-off-by: xedlag <145640114+xedlag@users.noreply.github.com>

--- backend/online_store/urls.py
+++ backend/online_store/urls.py
@@ -1,1 +1,11 @@
-from django.urls import path
+# urls.py
+# Django 项目的 URL 配置文件，定义 URL 路由。
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('store.urls')),
+]
+
