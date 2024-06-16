df82eafa9e5f944acf3808bf8cb618f0360a8d19
xedlag <145640114+xedlag@users.noreply.github.com>
2024-06-17 04:14:15 +0800

    Update wsgi.py
    Signed-off-by: xedlag <145640114+xedlag@users.noreply.github.com>

--- backend/online_store/wsgi.py
+++ backend/online_store/wsgi.py
@@ -1,1 +1,10 @@
+# wsgi.py
+# WSGI 配置文件，用于部署 Django 项目。
+
+import os
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_store.settings')
+
+application = get_wsgi_application()
+
